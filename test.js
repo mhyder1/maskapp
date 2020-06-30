89369386d68d06a510aeec9c69de4c225b1d8961
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-06-30 10:02:32 -0700

    edits

--- test.js
+++
@@ -1,8 +0,0 @@
-
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://openapi.etsy.com/v2/listings/active?api_key=l3a7mjohsu0hq3fbwq6p7pd3";
-   // fetch(proxyurl + url)
-    fetch(proxy + url)
-  .then(response => response.json())
-    
-  .then(items => console.log(items)) 
